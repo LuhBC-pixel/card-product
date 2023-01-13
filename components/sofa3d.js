e906c3122be3c4b9f89c4934ec896ca20ce426dc
Luisa Boina Coltro <bc.luisa@yahoo.com.br>
2023-01-13 18:33:11 -0300

    initial commit

---
+++ components/sofa3d.js
@@ -0,0 +1,24 @@
+export function Sofa3d() {
+  return (
+    <div class='sketchfab-embed-wrapper'>
+      {' '}
+      <iframe
+        title='Margot 2 Seater Sofa, Old Rose Velvet'
+        frameborder='0'
+        allowfullscreen
+        mozallowfullscreen='true'
+        webkitallowfullscreen='true'
+        allow='autoplay; fullscreen; xr-spatial-tracking'
+        xr-spatial-tracking
+        execution-while-out-of-viewport
+        execution-while-not-rendered
+        web-share
+        width='449'
+        height='253'
+        src='https://sketchfab.com/models/b3d693a003c94f4e989c08fe08c3af8c/embed?autostart=1&ui_animations=0&ui_infos=0&ui_stop=0&ui_inspector=0&ui_watermark_link=0&ui_watermark=0&ui_ar=0&ui_help=0&ui_settings=0&ui_vr=0&ui_fullscreen=0&ui_annotations=0'
+      >
+        {' '}
+      </iframe>{' '}
+    </div>
+  );
+}
--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,28 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang='pt-BR'>
+      <link rel='preconnect' href='https://fonts.googleapis.com' />
+      <link rel='preconnect' href='https://fonts.gstatic.com' />
+      <link
+        href='https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&display=swap'
+        rel='stylesheet'
+      />
+      <link
+        href='https://fonts.googleapis.com/css2?family=Crimson+Pro:wght@600&display=swap'
+        rel='stylesheet'
+      />
+      <script
+        type='module'
+        src='https://unpkg.com/@google/model-viewer/dist/model-viewer.min.js'
+      ></script>
+
       <Head />
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
